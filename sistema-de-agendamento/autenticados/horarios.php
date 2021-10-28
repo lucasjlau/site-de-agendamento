544d1aa98ff1efbe41bb81336143893a81408fa1
lucasjlau <lucasjlau2021@gmail.com>
2021-10-28 20:09:29 -0300

    sistema_agendamento0.1

--- sistema-de-agendamento/autenticados/horarios.php
+++ sistema-de-agendamento/autenticados/horarios.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Horarios</title>
-    <link rel="stylesheet" href="../css/hora.css">
+    <link rel="stylesheet" href="../css/horarios.css">
 </head>
 <body>
     <main>
