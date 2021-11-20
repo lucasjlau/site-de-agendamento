032c53d9f45cc25d93d91ab45466ad9cb6a0bdbf
lucasjlau <92949079+lucasjlau@users.noreply.github.com>
2021-11-20 14:05:01 -0300

    Update conexao.php

--- sistema-de-agendamento/funcoes/conexao.php
+++ sistema-de-agendamento/funcoes/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$banco = new mysqli("localhost", "root", "fenocriador", "barbearia");
+$banco = new mysqli("localhost", "root", " ", "barbearia");
 
 if ($banco->connect_errno):
     die("Erro (".$banco->connect_errno.") ".$banco->connect_error);
